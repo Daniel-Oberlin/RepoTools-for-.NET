060c09a29e3639df711efa0c20431aff8c48fe4e
Daniel Oberlin <doberlin@cambridgesoft.com>
2011-04-30 14:06:55 -0400

    Handle exceptions better with sync and cleanup unused temp directories

---
+++ Utilities/StreamUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    public class StreamUtilities
+    {
+        public static void CopyStream(Stream fromStream, Stream toStream, int chunkSize = 8192)
+        {
+            byte[] buffer = new byte[chunkSize];
+            int bytesRead = fromStream.Read(buffer, 0, chunkSize);
+			while (bytesRead > 0)
+			{
+                toStream.Write(buffer, 0, bytesRead);
+                bytesRead = fromStream.Read(buffer, 0, chunkSize);
+			}
+        }
+    }
+}
