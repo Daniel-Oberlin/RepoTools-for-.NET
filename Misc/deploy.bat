72a92ed609719de9e8009b50fbd330627646340c
Daniel Oberlin <doberlin@cambridgesoft.com>
2013-02-09 11:13:58 -0500

    Rename RepositoryDaemon to RepositoryServer, and other mostly cosmetic changes

--- Misc/deploy.bat
+++ Misc/deploy.bat
@@ -1,10 +1,10 @@
 copy C:\Users\doberlin\Dropbox\Personal\Repository\HttpServer\bin\Debug\HttpServer.dll C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryDaemon\bin\Debug\RepositoryDaemon.exe C:\Users\doberlin\Dropbox\Personal\bin
+copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryServer\bin\Debug\RepositoryServer.exe C:\Users\doberlin\Dropbox\Personal\bin
 copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryManifest\bin\Debug\RepositoryManifest.dll C:\Users\doberlin\Dropbox\Personal\bin
 copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositorySync\bin\Debug\RepositorySync.exe C:\Users\doberlin\Dropbox\Personal\bin
 copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryTool\bin\Debug\RepositoryTool.exe C:\Users\doberlin\Dropbox\Personal\bin
 copy C:\Users\doberlin\Dropbox\Personal\Repository\Utilities\bin\Debug\Utilities.dll C:\Users\doberlin\Dropbox\Personal\bin
 
 copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositorySync\bin\Debug\rs.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryDaemon\bin\Debug\red.exe C:\Users\doberlin\Dropbox\Personal\bin
+copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryServer\bin\Debug\repoServer.exe C:\Users\doberlin\Dropbox\Personal\bin
 copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryTool\bin\Debug\rt.exe C:\Users\doberlin\Dropbox\Personal\bin
