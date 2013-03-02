4cae4ccb649bb4bd95e0cec9381a0fc56cd06b21
Daniel Oberlin <doberlin@cambridgesoft.com>
2013-03-02 09:03:06 -0500

    update deployment script to reflect new location

--- Misc/deploy.bat
+++ Misc/deploy.bat
@@ -1,10 +1,10 @@
-copy C:\Users\doberlin\Dropbox\Personal\Repository\HttpServer\bin\Debug\HttpServer.dll C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryServer\bin\Debug\RepositoryServer.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryManifest\bin\Debug\RepositoryManifest.dll C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositorySync\bin\Debug\RepositorySync.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryTool\bin\Debug\RepositoryTool.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\Utilities\bin\Debug\Utilities.dll C:\Users\doberlin\Dropbox\Personal\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\HttpServer\bin\Debug\HttpServer.dll" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositoryServer\bin\Debug\RepositoryServer.exe" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositoryManifest\bin\Debug\RepositoryManifest.dll" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositorySync\bin\Debug\RepositorySync.exe" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositoryTool\bin\Debug\RepositoryTool.exe" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\Utilities\bin\Debug\Utilities.dll" C:\Users\doberlin\Dropbox\bin
 
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositorySync\bin\Debug\rs.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryServer\bin\Debug\repoServer.exe C:\Users\doberlin\Dropbox\Personal\bin
-copy C:\Users\doberlin\Dropbox\Personal\Repository\RepositoryTool\bin\Debug\rt.exe C:\Users\doberlin\Dropbox\Personal\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositorySync\bin\Debug\rs.exe" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositoryServer\bin\Debug\repoServer.exe" C:\Users\doberlin\Dropbox\bin
+copy "C:\Users\doberlin\Dropbox\Dev\Visual Studio\Repository\RepositoryTool\bin\Debug\rt.exe" C:\Users\doberlin\Dropbox\bin
